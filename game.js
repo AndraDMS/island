e791756b116a8147f95e91abc643cf48507817bd
Andy <andranadu@gmail.com>
2017-08-01 14:15:42 +0100

    Add files via upload

--- game.js
+++ game.js
@@ -1,13 +1,18 @@
 canvas = document.getElementById('world');
 ctx = canvas.getContext('2d');
-
 w = canvas.width;
 h = canvas.height;
 
 var imgdata = ctx.getImageData(0,0, w, h);
 var imgdatalen = imgdata.data.length;
 
-feig = 4.669201609102990671853203821578;
+canvasCloud = document.getElementById('world_Clouds');
+ctxCloud = canvasCloud.getContext('2d');
+wCloud = canvasCloud.width;
+hCloud = canvasCloud.height;
+
+
+//feig = 4.669201609102990671853203821578;
 
 function altCond(h,c,elem) {
 	this.h = (h?h:0)
@@ -16,7 +21,7 @@ function altCond(h,c,elem) {
 }
 
 var defColour = [29,50,88,255]
-var conds = [{h:-2,c:[23,66,145,255]},{h:0.0,c:[245,233,206,255]},{h:0.5,c:[97,184,72,255]},{h:2.2,c:[51,125,92,255]},{h:4.3,c:[63,93,80,255]},{h:7,c:[150,163,161,255]},{h:8,c:[206,236,245,255]}]
+var conds = [{h:-2.5,c:[23,66,145,255]},/*{h:0.0,c:[245,233,206,255]},*/{h:0.0,c:[97,184,72,255]},{h:2.2,c:[51,125,92,255]},{h:4.3,c:[63,93,80,255]},{h:7,c:[150,163,161,255]},{h:8,c:[206,236,245,255]}]
 
 pixelSize = 2;
 
@@ -39,47 +44,37 @@ function refresh() {
 
 function generate() {
 	console.log("Start: " + (new Date).getTime());
-	for (var i = 0; i <= w; i+=pixelSize) {
-		for (var j = 0; j <= h; j+=pixelSize) {
-			/*var mag = (noise.simplex2(i/100,j/100))*2;
-			mag += (noise.simplex2(i/50,j/50))
-			mag += (noise.simplex2(i/20,j/20))
-			mag += (noise.simplex2(i/5,j/5))/3
-			mag += (noise.perlin2(i/200,j/200))*5*/
+	for (var i = -w/2; i <= w/2; i+=pixelSize) {
+		for (var j = -h/2; j <= h/2; j+=pixelSize) {
 
-			var mag = 0;
-			var step = 1.3;
-			var weight = 1.2;
-			var size = 500;
-			var detail = 30;
-			var offset = -1000;
-			var flatten = 1.5;
+			var height = heightMap(i,j,1.3,1.2,500,30,-1000,1.5);
+			height-=Math.pow(dist(i,j)/(h/3),7);
+			//var step = 1.3;
+			//var weight = 1.2;
+			//var size = 500;
+			//var detail = 30;
+			//var offset = -1000;
+			//var flatten = 1.5;
 
-			//noise.seed(seed)
+			//for (var k = 1; k < detail; k++) {
+				//offset += seed + k*25;
+				//mag += (noise.simplex2((i-offset)*Math.pow(step,k)/size,(j-offset)*Math.pow(step,k)/size))*7/Math.pow(weight,k);
+			//}
 
-			for (var k = 1; k < detail; k++) {
-				offset += seed + k*25
-				//noise.seed(1+((seed+(k*256))%65536))
-				mag += (noise.simplex2((i-offset)*Math.pow(step,k)/size,(j-offset)*Math.pow(step,k)/size))*7/Math.pow(weight,k)
-			}
-
-			mag/=flatten;
-			mag-=Math.pow(dist(i-(w/2),j-(h/2))/(h/3),7);
+			//mag/=flatten;
 
 			var output = defColour;
 			//if (mag>=60) output = "rgb(" + mag + "," + mag + "," + mag + ")";
 			for (var k = 0; k < conds.length; k++) {
-				if (mag > conds[k].h) {
+				if (height > conds[k].h) {
 					output = conds[k].c
 				}
 			}
-			//ctx.fillStyle = output;
-			//ctx.fillRect(i, j, pixelSize, pixelSize);
 			for (var k = 0; k < Math.pow(pixelSize,2); k++) {
-				imgdata.data[4*((j+Math.floor(k/pixelSize))*w+i+(k%pixelSize))] = output[0];
-				imgdata.data[4*((j+Math.floor(k/pixelSize))*w+i+(k%pixelSize))+1] = output[1];
-				imgdata.data[4*((j+Math.floor(k/pixelSize))*w+i+(k%pixelSize))+2] = output[2];
-				imgdata.data[4*((j+Math.floor(k/pixelSize))*w+i+(k%pixelSize))+3] = output[3];
+				imgdata.data[4*((j+h/2+Math.floor(k/pixelSize))*w+i+w/2+(k%pixelSize))] = output[0];
+				imgdata.data[4*((j+h/2+Math.floor(k/pixelSize))*w+i+w/2+(k%pixelSize))+1] = output[1];
+				imgdata.data[4*((j+h/2+Math.floor(k/pixelSize))*w+i+w/2+(k%pixelSize))+2] = output[2];
+				imgdata.data[4*((j+h/2+Math.floor(k/pixelSize))*w+i+w/2+(k%pixelSize))+3] = output[3];
 			}
 		}
 	}
@@ -88,8 +83,98 @@ function generate() {
 	console.log("Print: " + (new Date).getTime());
 }
 
+function heightMap(i,j,step,weight,size,detail,offset,flatten) {
+	var mag = 0;
+
+	for (var k = 1; k < detail; k++) {
+		offset += seed + k*25;
+		mag += (noise.simplex2((i-offset)*Math.pow(step,k)/size,(j-offset)*Math.pow(step,k)/size))*7/Math.pow(weight,k);
+	}
+
+	mag/=flatten;
+	return mag;
+}
+
 function dist(x,y) {
 	return Math.pow(x*x+y*y,1/2)
 }
 
+toff = 0;
+function cloud() {
+	this.p = new vect([Math.random()*w-w/2,Math.random()*h-h/2]);
+	var randomTheta = (Math.random()*2-1)*Math.PI
+	this.v = new vect([Math.cos(randomTheta),Math.sin(randomTheta)])
+	this.a = new vect([0,0])
+	this.update = function () {
+		this.a = new vect(windspeed(this.p.v[0],this.p.v[1],toff))
+		this.v.add(this.a);
+		this.v.throttle(1);
+		this.p.add(this.v);
+		if (Math.abs(this.p.v[0]) > wCloud/2 || Math.abs(this.p.v[1]) > hCloud/2) {
+			this.p = new vect([Math.random()*w-w/2,Math.random()*h-h/2]);
+		}
+	}
+	this.draw = function () {
+		var x_ = Math.floor(this.p.v[0]);
+		var y_ = Math.floor(this.p.v[1]);
+		ctxCloud.fillStyle = "#fff";
+		ctxCloud.fillRect(x_+wCloud/2,y_+hCloud/2,pixelSize,pixelSize);
+	}
+}
+
+function windspeed(x,y,t) {
+	var windDir = noise.simplex3(x,y,t)*Math.PI
+	var windMag = noise.simplex3(x/300+6000,y/300+2000,t)/10
+	return [windMag*Math.cos(windDir),windMag*Math.sin(windDir)]
+}
+
+var clouds = []
+
+function addClouds(add) {
+	for (var i = 0; i < add; i++) {
+		clouds[clouds.length] = new cloud();
+	}
+}
+
+function updateClouds() {
+	toff+=0.01;
+	for (var i = 0; i < clouds.length; i++) {
+		clouds[i].update();
+	}
+	ctxCloud.clearRect(0, 0, wCloud, hCloud);
+	for (var i = 0; i < clouds.length; i++) {
+		clouds[i].draw();
+	}
+}
+
+function cloudAnimTemp() {
+	updateClouds();
+	requestAnimationFrame(cloudAnimTemp);
+}
+
 generate();
+
+function vect(v) {
+	this.v = (v?v:[])
+	this.mag = function () {
+		var sum = 0;
+		for (var i = 0; i < this.v.length; i++) {
+			sum+=Math.pow(this.v[i],2);
+		}
+		return Math.pow(sum,1/2);
+	}
+	this.add = function (vect,coef) {
+		if (!coef) coef = 1;
+		for (var i = 0; i < this.v.length; i++) {
+			this.v[i]+=vect.v[i]*coef
+		}
+	}
+	this.throttle = function(speed) {
+		var currMag = this.mag();
+		if (currMag > speed) {
+			for (var i = 0; i < this.v.length; i++) {
+				this.v[i]*=speed/currMag
+			}
+		}
+	}
+}
